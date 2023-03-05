6147d62ee0eb3ae5dadf1a831ee6dd2f14e6175c
vijayverma20 <vijaykumar.verma20@gmail.com>
2023-03-05 21:14:32 +0800

    Add files via upload

---
+++ App.razor.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Radzen;
+
+namespace SEB.OutpaymentSystem.Client
+{
+    public partial class App
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public IJSRuntime jsRuntime { get; set; }
+
+        [Inject]
+        NotificationService NotificationService { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            base.OnInitialized();
+            if (!NavigationManager.BaseUri.Contains("localhost"))
+            {
+                await RegisterForUpdateAvailableNotification();
+            }
+        }
+
+        private async Task RegisterForUpdateAvailableNotification()
+        {
+            await jsRuntime.InvokeAsync<object>(
+                identifier: "registerForUpdateAvailableNotification",
+                DotNetObjectReference.Create(this),
+                nameof(OnUpdateAvailable));
+        }
+
+        [JSInvokable(nameof(OnUpdateAvailable))]
+        public Task OnUpdateAvailable()
+        {
+            NotificationService.Notify(
+                new NotificationMessage()
+                {
+                    Severity = NotificationSeverity.Info,
+                    Summary = "Update available",
+                    Detail = "A new version of the application is available. Click here to reload.",
+                    Click = async (not) => await jsRuntime.InvokeVoidAsync("reloadApp"),
+                    CloseOnClick = false,
+                    Duration = 60 * 60 * 1000
+                });
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ service-worker.published.js
@@ -0,0 +1,66 @@
+// Caution! Be sure you understand the caveats before publishing an application with
+// offline support. See https://aka.ms/blazor-offline-considerations
+
+self.importScripts('./service-worker-assets.js');
+self.addEventListener('install', event => event.waitUntil(onInstall(event)));
+self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
+self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
+
+const cacheNamePrefix = 'offline-cache-';
+//const CACHE_VERSION = '%%CACHE_VERSION%%'
+const CACHE_VERSION = '1.0.7'
+const cacheName = `${cacheNamePrefix}${CACHE_VERSION}`
+//const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
+const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/];
+const offlineAssetsExclude = [/^service-worker\.js$/];
+const integrityExclude = [/\.ico/];
+
+async function onInstall(event) {
+    console.info('Service worker: Install');
+    self.skipWaiting();
+    // Fetch and cache all matching items from the assets manifest
+    const assetsRequests = self.assetsManifest.assets
+        .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
+        .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
+        .map(asset => {
+            // Start of new code
+            const integrity =
+                integrityExclude.some(pattern => pattern.test(asset.url))
+                    ? null
+                    : asset.hash;
+
+            return !!integrity
+                ? new Request(asset.url, { integrity })
+                : new Request(asset.url);
+            // End of new code
+        });
+
+    assetsRequests.forEach(async el => {
+        await caches.open(cacheName).then(cache => cache.add(el));
+    });
+}
+
+async function onActivate(event) {
+    console.info('Service worker: Activate');
+
+    // Delete unused caches
+    const cacheKeys = await caches.keys();
+    await Promise.all(cacheKeys
+        .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
+        .map(key => caches.delete(key)));
+}
+
+async function onFetch(event) {
+    let cachedResponse = null;
+    if (event.request.method === 'GET') {
+        // For all navigation requests, try to serve index.html from cache
+        // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
+        const shouldServeIndexHtml = event.request.mode === 'navigate';
+
+        const request = shouldServeIndexHtml ? 'index.html' : event.request;
+        const cache = await caches.open(cacheName);
+        cachedResponse = await cache.match(request);
+    }
+
+    return cachedResponse || fetch(event.request);
+}
